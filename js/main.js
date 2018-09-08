d63b65411925462b4d470abceee23c904ac06128
rjdeus <41559152+rjdeus@users.noreply.github.com>
2018-09-07 22:38:07 -0400

    Changed photo and names

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Ryan Deus.", "An Engineer.", "A Savant."],
       typeSpeed: 100,
       loop: true,
     });
